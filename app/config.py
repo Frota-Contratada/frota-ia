4a65c85cb47f7e8f5d5f2522abd2352498667999
SofiaRSantana <sofia.santana@institutojef.org.br>
2026-08-19 08:43:20 -0300

    raw: change api key - Gemini to Groq

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 from pathlib import Path
 
 class Settings(BaseSettings):
-    gemini_api_key: str | None = None
+    groq_api_key: str | None = None
     grok_api_key: str | None = None
 
     class Config:
